9120ff1d14279d68e53e3f5a232c4ff3c6c9baf6
mahenv <mahendrauppari044@gmail.com>
2024-11-11 15:01:50 -0500

    nextjs app setup and clerk provider setyp

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { ModeToggle } from "./ModeToggle";
+
+export default function Home() {
+  let x;
+  return (
+    <div>
+      <Button>Hey</Button>
+      <ModeToggle/>
+    </div>
+  );
+}
